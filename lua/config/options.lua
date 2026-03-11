ee5ea6679374009d7680e1fb91f97485099dd9c1
Maximilian Achenbach <maximiliana28@gmail.com>
2026-03-11 19:11:12 +0100

    feat: Add wsl-clipboard functionality

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,21 @@ vim.opt.softtabstop = 4    -- Number of spaces for <Tab> in insert mode
 vim.opt.smartindent = true -- Auto-indent new lines
 vim.opt.number = true
 vim.opt.relativenumber = true -- relative line  numbers
+
+-- wsl clipboard
+local in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+
+if in_wsl then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
